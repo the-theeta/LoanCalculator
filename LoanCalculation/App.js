1a913cead37030e06fcd22f25f7eb6e878d8a7dd
Yatish Dhanani <yatishd@solutelabs.com>
2020-06-04 16:58:58 +0530

    App added

---
+++ LoanCalculation/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import AmortizedLoan from './src/components/AmortizedLoan';
+
+const App = () => {
+  return (
+      <AmortizedLoan />
+  );
+};
+
+export default App;
